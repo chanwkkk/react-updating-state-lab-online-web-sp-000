d7b094db6277a9412ad535cab18b85bf2bc701d5
Yingqi Chen <chanwk18@hotmail.com>
2020-02-15 22:03:39 +0000

    Automatically backed up by Learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -2,12 +2,13 @@
 import React from 'react'
 
 export default class YouTubeDebugger extends React.Component{
+  
 
 
     render(){
         return(
-     <button className = "bitrate" onClick = {this.handleBitrate}>Bitrate</button>
-     <button className = "resolution" onClick = {this.handleResolution}>Bitrate</button>
+           <button className = "bitrate" onClick = {this.handleBitrate}>Bitrate</button>
+           <button className = "resolution" onClick = {this.handleResolution}>Bitrate</button>
 
         )
     }
