6f828fded6eb1c84fc522491c34447ab71caafba
Yingqi Chen <chanwk18@hotmail.com>
2020-02-15 22:11:01 +0000

    Automatically backed up by Learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -19,7 +19,9 @@ export default class YouTubeDebugger extends React.Component{
   }
 
   handleBitrate = () =>{
-
+    this.setState({
+      settings: object.assign({},this.state.settings, {}))
+    }
   }
 
   handleResolution = () =>{
