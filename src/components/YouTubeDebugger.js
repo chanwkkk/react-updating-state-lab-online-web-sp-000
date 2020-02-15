8153481d0f822f1bd93fc408200b7b317d128515
Yingqi Chen <chanwk18@hotmail.com>
2020-02-15 22:07:46 +0000

    Automatically backed up by Learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -3,6 +3,21 @@ import React from 'react'
 
 export default class YouTubeDebugger extends React.Component{
 
+  constructor(){
+    super();
+    this.state = {
+      errors: [],
+      user: null,
+      settings: {
+        bitrate: 8,
+        video: {
+          resolution: '1080p'
+        }
+      }
+}
+    
+  }
+
   handleBitrate = () =>{
 
   }
