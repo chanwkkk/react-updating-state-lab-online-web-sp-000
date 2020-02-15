9b46f1cf4fb0c3123ae931480d4dcf42736fd0ea
Yingqi Chen <chanwk18@hotmail.com>
2020-02-15 22:12:19 +0000

    Automatically backed up by Learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -21,8 +21,8 @@ export default class YouTubeDebugger extends React.Component{
   handleBitrate = () =>{
     this.setState({
       settings: Object.assign({},this.state.settings, {bitrate: 12}))
-    }
-  )
+    })
+  }
 
   handleResolution = () =>{
 
