85a8a484517df6ee3ed56f49c71b6f036a1c6a4c
Yingqi Chen <chanwk18@hotmail.com>
2020-02-15 22:06:07 +0000

    Automatically backed up by Learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -7,9 +7,10 @@ export default class YouTubeDebugger extends React.Component{
 
     render(){
         return(
+        <div>
            <button className = "bitrate" onClick = {this.handleBitrate}>Bitrate</button>
            <button className = "resolution" onClick = {this.handleResolution}>Bitrate</button>
-
+       </div>
         )
     }
 
