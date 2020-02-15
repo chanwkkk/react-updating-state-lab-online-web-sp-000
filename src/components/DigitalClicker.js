d60e5b249a1eac6ac5baa5a373373129063057d6
Yingqi Chen <chanwk18@hotmail.com>
2020-02-15 22:20:07 +0000

    Automatically backed up by Learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,20 @@
 // Code DigitalClicker Component Here
+import React from 'react'
+
+export default class DigitalClicker extends React.Component{
+   constructor(){
+       super()
+       this.state = {
+          timesClicked: 0
+       }
+  } 
+
+    handleClick = () =>{
+        this.setState(state => {timesClicked: state+1})
+    }
+    
+    render(){
+    <button onClick = {this.handleClick}>{this.state.timesClicked}</button>
+
+    }
+}
